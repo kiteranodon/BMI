4da11271db11d70fc6d94d8848a341f510ee3095
Kaisei <tidakakaka12@gmail.com>
2025-11-05 17:13:51 +0900

    単位変換、bmi計算完了

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -8,39 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var typeOfNumber = "height"
-    @State private var height: Float? = nil
-    @State private var weight: Float? = nil
-    
+    @State private var typeOfNumber = "meter"
     let typesOfNumber  = ["meter", "centimeter"]
+    
+    @State private var height: String = ""
+    @State private var weight: String = ""
+    
+    // 計算プロパティで型変換
+    var heightNumber: Float? {
+        guard let value = Float(height) else { return nil }
+            if typeOfNumber == "centimeter" {
+                return value / 100.0
+            } else {
+                return value // meter の想定
+            }
+        }
+    var weightNumber: Float? {
+        Float(weight)
+    }
+    
+    var bmi: Float? {
+        guard let h = heightNumber, let w = weightNumber, h > 0 else { return nil }
+               return w / (h * h)
+           }
+    
+    
+    
     var body: some View {
         NavigationStack{
             Form{
                 Section("入力"){
                     HStack{
-                        TextField("身長", value: $height, format: .number)
-                            .keyboardType(.decimalPad)
+                        TextField("身長", text: $height)
                         Section(""){
                             // Pickerを配置
                             Picker("", selection: $typeOfNumber) {
                                 ForEach(typesOfNumber, id: \.self) { type in
                                     Text(type)
-                                        .foregroundStyle(.blue) // Pickerの中の文字を青色に
                                 }
                             }
                         }
                     }
                     
                     HStack{
-                        TextField("体重", value: $weight, format: .number)
-                            .keyboardType(.decimalPad)
+                        TextField("体重", text: $weight)
                         Text("kg")
-                    
+                        
                     }
                 }
                 
-                Section("結果"){
-                    Text("BMI")
+                Section("結果") {
+                    if let bmi = bmi {
+                        Text("BMI: \(String(format: "%.2f", Double(bmi)))")
+                    } else {
+                        Text("正しい数値を入力してください")
+                    }
                 }
             }
         }
